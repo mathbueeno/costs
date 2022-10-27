7aec5ed37a4b4b4a8d68c447ccff9efa2523c269
mathbueeno <mathbueeno@gmail.com>
2022-10-27 15:01:30 -0300

    Finalização do Footer

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,6 +1,17 @@
+import { FaFacebook, FaInstagram, FaLinkedin } from 'react-icons/fa'
+
+import styles from '../layout/footer.module.css';
+
 function Footer() {
     return (
-        <p>Footer</p>
+        <footer className={styles.footer}>
+            <ul className={styles.social_list}>
+                <li><FaFacebook /></li>
+                <li><FaInstagram /></li>
+                <li><FaLinkedin /></li>
+            </ul>
+            <p className={styles.copy_right}><span>Costs</span> &copy; 2022</p>
+        </footer>
     )
 }
 export default Footer
